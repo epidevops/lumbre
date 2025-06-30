11f85567e719b8adee2c98bebb6b559eaf655c4b
epidevops <epidevops@gmail.com>
2025-06-30 16:22:57 -0600

    Feat: Add Channel

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,6 +1,5 @@
 class Contact < ApplicationRecord
-  include Avatar, EmailValidations, NoticedAssociations
-  has_person_name
+  include EmailValidations, NoticedAssociations
   has_many :contact_messages, dependent: :destroy
   accepts_nested_attributes_for :contact_messages
 end
