9246e041caef50a5e485775fb33071efa6494ac9
epidevops <epidevops@gmail.com>
2025-07-08 14:58:33 -0600

    Feat: OTP For Admin Users

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -4,5 +4,11 @@
 # Use this to limit dissemination of sensitive information.
 # See the ActiveSupport::ParameterFilter documentation for supported notations and behaviors.
 Rails.application.config.filter_parameters += [
-  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc, :otp_attempt, :otp_secret, :otp_backup_code
+  :passw, :email, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn, :cvv, :cvc, :otp_attempt, :otp_secret, :otp_backup_codes,
+  # Additional security filters for admin tools
+  :query, :sql, :statement, # Blazer query content
+  :exception, :error_message, :backtrace, # Exception Track sensitive data
+  :session_id, :csrf_token, :authenticity_token, # Session security
+  :api_key, :access_token, :refresh_token, # API tokens
+  :private_key, :public_key, :certificate_key # Certificates
 ]
