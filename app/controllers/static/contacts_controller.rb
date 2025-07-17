6111eb31d6732303ad6d5ec21c9cacba683bc5a8
epidevops <epidevops@gmail.com>
2025-07-17 15:59:52 -0600

    Feat: Add rate_limit and fix tests

--- app/controllers/static/contacts_controller.rb
+++ app/controllers/static/contacts_controller.rb
@@ -1,5 +1,6 @@
 class Static::ContactsController < ApplicationController
-  rate_limit to: 3, within: 2.seconds, by: -> { request.domain }, with: -> { redirect_to root_path, alert: "Try again later." }, only: :create
+  # rate_limit to: 10, within: 3.minutes, by: -> { request.domain }, with: -> { redirect_to root_path, alert: "Try again later." }, only: :create
+  rate_limit to: 10, within: 3.minutes, only: :create, with: -> { redirect_to root_url, alert: "Try again later." }
   CONTACT_MESSAGE_SUCCESS_MESSAGE = "Your message has been sent. We will get back to you as soon as possible."
 
   def create
@@ -11,20 +12,18 @@ class Static::ContactsController < ApplicationController
       @contact.contact_messages.build(message: contact_params[:contact_messages_attributes]["0"][:message])
     end
 
-    flash.now[:notice] = CONTACT_MESSAGE_SUCCESS_MESSAGE
-
     if @contact.save
       NewContactUsContactNotifier.with(record: @contact, email: @contact.email, inquiry: @contact.contact_messages.last.message).deliver(@contact)
       NewContactUsAdminNotifier.with(record: @contact, email: @contact.email, inquiry: @contact.contact_messages.last.message).deliver(AdminUser.super_admin_users)
       respond_to do |format|
         format.html { redirect_to root_path, notice: CONTACT_MESSAGE_SUCCESS_MESSAGE }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = CONTACT_MESSAGE_SUCCESS_MESSAGE }
       end
     else
       logger.error("Contact message creation failed: #{@contact.errors.full_messages}")
       respond_to do |format|
         format.html { redirect_to root_path, notice: CONTACT_MESSAGE_SUCCESS_MESSAGE }
-        format.turbo_stream
+        format.turbo_stream { flash.now[:notice] = CONTACT_MESSAGE_SUCCESS_MESSAGE }
       end
     end
   end
