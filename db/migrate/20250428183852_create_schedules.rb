43a2276ed471bc7f37866beeca5909d85827a3b8
epidevops <epidevops@gmail.com>
2025-08-06 20:27:14 -0600

    Feat: Schedules Rules to Flowbite

--- db/migrate/20250428183852_create_schedules.rb
+++ db/migrate/20250428183852_create_schedules.rb
@@ -4,12 +4,12 @@ class CreateSchedules < ActiveRecord::Migration[8.0]
       t.references :scheduleable, polymorphic: true, null: false
       t.string :name, null: false
       t.boolean :active, null: false, default: false
-      t.integer :capacity, default: 1
-      t.boolean :exclude_lunch_time, default: true
+      t.integer :capacity, default: 5
+      t.boolean :exclude_lunch_time, default: false
       t.string :lunch_hour_start
       t.string :lunch_hour_end
-      t.string :beginning_of_week, default: "monday"
-      t.string :time_zone
+      t.string :beginning_of_week, default: "sunday"
+      t.string :time_zone, null: false, default: "Mountain Time (US & Canada)"
 
       t.timestamps
     end
--- db/migrate/20250428185205_create_rules.rb
+++ db/migrate/20250428185205_create_rules.rb
@@ -2,10 +2,10 @@ class CreateRules < ActiveRecord::Migration[8.0]
   def change
     create_table :rules do |t|
       t.belongs_to :schedule, null: false, foreign_key: true
-      t.string :rule_type
-      t.string :name
-      t.string :frequency_units
-      t.integer :frequency
+      t.string :rule_type, null: false, default: "inclusion"
+      t.string :name, null: false
+      t.string :frequency_units, null: false, default: "IceCube::MinutelyRule"
+      t.integer :frequency, null: false, default: 15
       t.json :days_of_week, default: [], null: false
       t.date :start_date
       t.date :end_date
