3eee4001bec4beb39c18c6ef188c9dfbb7ee71f0
epidevops <epidevops@gmail.com>
2025-08-14 17:35:52 -0600

    Feat: Dashboard Update

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,40 +4,88 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "px-4 py-16 md:py-32 text-center m-auto max-w-3xl" do
-      h2 "Welcome to ActiveAdmin", class: "text-base font-semibold leading-7 text-indigo-600 dark:text-indigo-500"
-      para "This is the default page", class: "mt-2 text-3xl sm:text-4xl font-bold text-gray-900 dark:text-gray-200"
-      para class: "mt-6 text-xl leading-8 text-gray-700 dark:text-gray-400" do
-        text_node "To update the content, edit the"
-        strong "app/admin/dashboard.rb"
-        text_node "file to get started."
-      end
+    card_data = [
+      {
+        title: "Manage Restaurant",
+        description: "This is where you can configure internationalization, socials, addreses, events, and more.",
+        link: "/admin/restaurants/1/edit"
+      },
+      {
+        title: "Manage Products",
+        description: "This is where you can configure products, categories, and more.",
+        link: "/admin/products",
+        image: "/docs/images/blog/image-4.jpg"
+      },
+      {
+        title: "Manage Me",
+        description: "View and edit your account, change your password, and more.",
+        link: "/admin/admin_users/#{current_admin_user.id}/edit",
+        image: "/docs/images/blog/image-4.jpg"
+      }
+    ]
+    div class: "grid grid-cols-2 md:grid-cols-3 gap-8" do
+      render partial: "card", collection: card_data
     end
 
-    div class: "px-4 pb-8 m-auto max-w-3xl" do
-      para "Url Options", class: "mt-2 text-3xl sm:text-4xl font-bold text-gray-900 dark:text-gray-200 text-center"
-      ul class: "mt-6 text-xl leading-8 text-gray-700 dark:text-gray-400 list-none p-0 text-left whitespace-nowrap" do
-        li "ActiveStorage::Current.url_options: #{ActiveStorage::Current.url_options}"
-        li "ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }"
-        li "protocol: #{request.protocol}, host: #{request.host}, port: #{request.port}"
-        li "Rails.application.routes.default_url_options: #{Rails.application.routes.default_url_options}"
-      end
-    end
 
-    panel "Admin Tools" do
-      if Flipper.enabled?(:super_admin_access, current_admin_user)
-        h3 { b { link_to("Blazer", mount_blazer_path, target: "_blank") } }
-        h3 { b { link_to("Litequeen", mount_solid_litequeen_path, target: "_blank") } }
-        h3 { b { link_to("Mission Control Jobs", mount_mission_control_jobs_path, target: "_blank") } }
-        h3 { b { link_to("Letter Opener", mount_letter_opener_web_path, target: "_blank") } }
-        h3 { b { link_to("Lookbook", mount_lookbook_path, target: "_blank") } }
-        h3 { b { link_to("Exception Track", mount_exception_track_path, target: "_blank") } }
-      end
-      h3 { b { link_to("Flipper", "/flipper", target: "_blank") } }
-      # h3 { b { link_to('Model Graph', '/models') } }
-      # h3 { b { link_to('PgHero', '/pghero') } }
-      # h3 { b { link_to('Sidekiq', '/sidekiq') } }
-      # h3 { b { link_to('Vue Playground', '/vue-playground') } }
-    end
+    # div class: "px-4 py-16 md:py-32 text-center m-auto max-w-3xl" do
+    #   h2 "Welcome to ActiveAdmin", class: "text-base font-semibold leading-7 text-indigo-600 dark:text-indigo-500"
+    #   para "This is the default page", class: "mt-2 text-3xl sm:text-4xl font-bold text-gray-900 dark:text-gray-200"
+    #   para class: "mt-6 text-xl leading-8 text-gray-700 dark:text-gray-400" do
+    #     text_node "To update the content, edit the"
+    #     strong "app/admin/dashboard.rb"
+    #     text_node "file to get started."
+    #   end
+    # end
+
+    # div class: "px-4 pb-8 m-auto max-w-3xl" do
+    #   para "Url Options", class: "mt-2 text-3xl sm:text-4xl font-bold text-gray-900 dark:text-gray-200 text-center"
+    #   ul class: "mt-6 text-xl leading-8 text-gray-700 dark:text-gray-400 list-none p-0 text-left whitespace-nowrap" do
+    #     li "ActiveStorage::Current.url_options: #{ActiveStorage::Current.url_options}"
+    #     li "ActiveStorage::Current.url_options = { protocol: request.protocol, host: request.host, port: request.port }"
+    #     li "protocol: #{request.protocol}, host: #{request.host}, port: #{request.port}"
+    #     li "Rails.application.routes.default_url_options: #{Rails.application.routes.default_url_options}"
+    #   end
+    # end
+
+    # panel "Cards Partial" do
+    #   div class: "grid grid-cols-2 md:grid-cols-3 gap-4" do
+    #     5.times do
+    #       render partial: "card"
+    #     end
+    #   end
+    # end
+
+    # panel "Admin Tools" do
+    #   if Flipper.enabled?(:super_admin_access, current_admin_user)
+    #     h3 { b { link_to("Blazer", mount_blazer_path, target: "_blank") } }
+    #     h3 { b { link_to("Litequeen", mount_solid_litequeen_path, target: "_blank") } }
+    #     h3 { b { link_to("Mission Control Jobs", mount_mission_control_jobs_path, target: "_blank") } }
+    #     h3 { b { link_to("Letter Opener", mount_letter_opener_web_path, target: "_blank") } }
+    #     h3 { b { link_to("Lookbook", mount_lookbook_path, target: "_blank") } }
+    #     h3 { b { link_to("Exception Track", mount_exception_track_path, target: "_blank") } }
+    #   end
+    #   h3 { b { link_to("Flipper", "/flipper", target: "_blank") } }
+    #   # h3 { b { link_to('Model Graph', '/models') } }
+    #   # h3 { b { link_to('PgHero', '/pghero') } }
+    #   # h3 { b { link_to('Sidekiq', '/sidekiq') } }
+    #   # h3 { b { link_to('Vue Playground', '/vue-playground') } }
+    # end
+
+    # panel "Admin Tools" do
+    #   if Flipper.enabled?(:super_admin_access, current_admin_user)
+    #     h3 { b { link_to("Blazer", mount_blazer_path, target: "_blank") } }
+    #     h3 { b { link_to("Litequeen", mount_solid_litequeen_path, target: "_blank") } }
+    #     h3 { b { link_to("Mission Control Jobs", mount_mission_control_jobs_path, target: "_blank") } }
+    #     h3 { b { link_to("Letter Opener", mount_letter_opener_web_path, target: "_blank") } }
+    #     h3 { b { link_to("Lookbook", mount_lookbook_path, target: "_blank") } }
+    #     h3 { b { link_to("Exception Track", mount_exception_track_path, target: "_blank") } }
+    #   end
+    #   h3 { b { link_to("Flipper", "/flipper", target: "_blank") } }
+    #   # h3 { b { link_to('Model Graph', '/models') } }
+    #   # h3 { b { link_to('PgHero', '/pghero') } }
+    #   # h3 { b { link_to('Sidekiq', '/sidekiq') } }
+    #   # h3 { b { link_to('Vue Playground', '/vue-playground') } }
+    # end
   end
 end
